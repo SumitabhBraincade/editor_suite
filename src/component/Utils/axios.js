8c664adb9d8fb00d6cc16dfe295794ac6b5377fa
SumitabhBraincade <sumitabh@braincade.in>
2024-07-27 12:11:15 +0530

    removed_with_credentials

--- src/component/Utils/axios.js
+++ src/component/Utils/axios.js
@@ -3,12 +3,13 @@ import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
   baseURL: "https://console.braincade.in/backend",
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
     const userToken = Cookies.get("access_token");
+    console.log(userToken);
     if (userToken) {
       config.headers.Authorization = `Bearer ${userToken}`;
     }
